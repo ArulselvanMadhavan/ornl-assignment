8a72cbff0c62957c0dfe49b0e386b7f234ae7a3e
Arulselvan Madhavan <arulselvan1234@gmail.com>
2023-03-03 11:01:37 -0800

    First unit test done

--- test/check.f90
+++ test/check.f90
@@ -13,15 +13,83 @@ contains
       !> Collection of tests
       type(unittest_type), allocatable, intent(out) :: testsuite(:)
 
-      testsuite = [ new_unittest("valid", test_valid) ]
+      testsuite = [new_unittest("random_string(length=0)", test_random_0), &
+                   new_unittest("random_string(length=5)", test_random_5), &
+                   new_unittest("random_string(length=50)", test_random_50), &
+                   new_unittest("random_string(length=500)", test_random_500) &
+                   ]
 
    end subroutine collect_suite1
 
-   subroutine test_valid(error)
+   subroutine test_random_5(error)
       type(error_type), allocatable, intent(out) :: error
-      call check(error, extract_digits(".7324"), "7324")
+      call test_random_string(error, 5)
+   end subroutine test_random_5
+
+   subroutine test_random_0(error)
+      type(error_type), allocatable, intent(out) :: error
+      call test_random_string(error, 0)
+   end subroutine test_random_0
+
+   subroutine test_random_50(error)
+      type(error_type), allocatable, intent(out) :: error
+      call test_random_string(error, 50)
+   end subroutine test_random_50
+
+   subroutine test_random_500(error)
+      type(error_type), allocatable, intent(out) :: error
+      call test_random_string(error, 500)
+   end subroutine test_random_500
+
+   subroutine random_word(word, digits_only_word)
+      use stdlib_random, only: dist_rand
+      use stdlib_kinds, only: int8, sp
+      integer(int8) :: zero_ascii, nine_ascii, digit_range, ascii_code, sample
+      character(len=*), intent(inout) :: word
+      character(len=len(word)) :: digits
+      character(len=:), allocatable, intent(out) :: digits_only_word
+      integer :: i, count, pos
+      real(sp) :: sample_fp32
+      ! Init
+      zero_ascii = 48
+      nine_ascii = 57
+      digit_range = nine_ascii - zero_ascii + 1
+      count = 0
+
+      ! Word gen
+      do i = 1, len(word)
+         sample = dist_rand(1_int8)
+
+         ! Positive range
+         if (sample < 0) then
+            sample = sample + 128
+         end if
+         ! Insert a digit about 50% of the time
+         if (sample < 128/2) then
+            pos = count + 1
+            sample_fp32 = sample/128.0
+            ascii_code = zero_ascii + floor(digit_range*sample_fp32)
+            count = count + 1
+            digits(pos:pos) = char(ascii_code)
+         else
+            ascii_code = sample
+         end if
+
+         word(i:i) = char(ascii_code)
+      end do
+      digits_only_word = digits(1:count)
+   end subroutine random_word
+
+   subroutine test_random_string(error, size)
+      type(error_type), allocatable, intent(out) :: error
+      integer, intent(in) :: size
+      integer, parameter :: total_tests = 2
+      character(len=size) :: word
+      character(len=:), allocatable :: digits_only_word
+      call random_word(word, digits_only_word)
+      call check(error, extract_digits(word), digits_only_word)
       if (allocated(error)) return
-   end subroutine test_valid
+   end subroutine test_random_string
 
 end module test_suite1
 
