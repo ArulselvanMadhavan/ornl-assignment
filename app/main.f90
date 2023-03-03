921297291b2d1256cbc076e84e6b9e44bb99aecb
Arulselvan Madhavan <arulselvan1234@gmail.com>
2023-03-03 02:04:59 -0800

    string_type seems to work

--- app/main.f90
+++ app/main.f90
@@ -1,6 +1,8 @@
 program main
   use ornl_assignment, only: say_hello
+  use stdlib_string_type, only: string_type, assignment(=)
   implicit none
-
-  call say_hello("7199")
+  type(string_type) :: akeys
+  akeys = "7199"
+  call say_hello(akeys)
 end program main
--- src/ornl-assignment.f90
+++ src/ornl-assignment.f90
@@ -1,36 +1,40 @@
-module map_functions
-use iso_fortran_env, only:int8, int16, int32, int64, real32, real64, real128
-  
-  implicit none
-  public
-  contains
-  pure logical function is_number(x) result(res)
-    character, intent(in) :: x
-    res = .true.
-  end function is_number
+! module map_functions
+! use iso_fortran_env, only:int8, int16, int32, int64, real32, real64, real128
+! use stdlib_string_type  
+!   implicit none
+!   public
+!   contains
+!   pure logical function is_number(x) result(res)
+!     character, intent(in) :: x
+!     res = .true.
+!   end function is_number
 
-pure integer(int8) function xpowx_i1(x) result(res)
-  integer(int8), intent(in) :: x
-  res = x**x
-end function xpowx_i1
+! pure integer(int8) function xpowx_i1(x) result(res)
+!   integer(int8), intent(in) :: x
+!   res = x**x
+! end function xpowx_i1
 
-end module map_functions
+! end module map_functions
 
 module ornl_assignment
-  use functional
-  use map_functions
   implicit none
   private
 
   public :: say_hello
 contains
-  subroutine say_hello(akeys)
-    implicit none
-    character(len=*), intent(in) :: akeys
-    akeys = map(xpowx_i1, akeys)
-    ! do n = 0, len(akeys)
-    !    print *, "Key:", akeys(n)
-    ! end do
-    print *, "Submission by: ", len(akeys)
+  subroutine say_hello(string)
+  use stdlib_string_type, only: string_type, assignment(=), write (formatted)
+  use stdlib_strings, only: padr
+  implicit none
+  type(string_type), intent(in) :: string
+
+  ! string = "right pad this string"
+! string <-- "right pad this string"
+
+  print '(dt)', padr(string, 25, "$") ! "right pad this string$$$$"
+
+  !string = padr(string, 25)
+  print '(dt)', string
+! string <-- "right pad this string    "
   end subroutine say_hello
 end module ornl_assignment
