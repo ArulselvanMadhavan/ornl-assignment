c7e31fa969d3a2155bce74d369e1329e869e2d02
Arulselvan Madhavan <arulselvan1234@gmail.com>
2023-03-03 09:37:07 -0800

    First unit test

--- Makefile
+++ Makefile
@@ -8,3 +8,6 @@ build:
 
 run:
 	@fpm run
+
+test:
+	@fpm test
--- test/check.f90
+++ test/check.f90
@@ -1,5 +1,52 @@
-program check
+module test_suite1
+   use testdrive, only: new_unittest, unittest_type, error_type, check
+   use ornl_assignment, only: extract_digits
    implicit none
+   private
 
-   print *, "Put some tests in here!"
+   public :: collect_suite1
+
+contains
+
+!> Collect all exported unit tests
+   subroutine collect_suite1(testsuite)
+      !> Collection of tests
+      type(unittest_type), allocatable, intent(out) :: testsuite(:)
+
+      testsuite = [ new_unittest("valid", test_valid) ]
+
+   end subroutine collect_suite1
+
+   subroutine test_valid(error)
+      type(error_type), allocatable, intent(out) :: error
+      call check(error, extract_digits(".7324"), "7324")
+      if (allocated(error)) return
+   end subroutine test_valid
+
+end module test_suite1
+
+program check
+   use, intrinsic :: iso_fortran_env, only: error_unit
+   use testdrive, only: run_testsuite, new_testsuite, testsuite_type
+   use test_suite1, only: collect_suite1
+   implicit none
+   integer :: stat, is
+   type(testsuite_type), allocatable :: testsuites(:)
+   character(len=*), parameter :: fmt = '("#", *(1x, a))'
+
+   stat = 0
+
+   testsuites = [ &
+                new_testsuite("suite1", collect_suite1) &
+                ]
+
+   do is = 1, size(testsuites)
+      write (error_unit, fmt) "Testing:", testsuites(is)%name
+      call run_testsuite(testsuites(is)%collect, error_unit, stat)
+   end do
+
+   if (stat > 0) then
+      write (error_unit, '(i0, 1x, a)') stat, "test(s) failed!"
+      error stop
+   end if
 end program check
