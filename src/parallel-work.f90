643328617a5db53a490b144097af4eb7748d20d8
Arulselvan Madhavan <arulselvan1234@gmail.com>
2023-03-04 08:39:32 -0800

    Rm

--- src/parallel-work.f90
+++ src/parallel-work.f90
@@ -13,24 +13,21 @@ contains
       character(len=word_len) :: word
       character(len=:), allocatable :: digits_in_word
       character(len=word_len + 3), allocatable :: words(:) [:]
-      integer, allocatable :: word_lens(:) [:]
 
       from = (image_id - 1)*problem_size + 1
       to = image_id*problem_size
       allocate (words(from:to) [*])
-      allocate (word_lens(from:to) [*])
 
       do i = 0, problem_size - 1
          call random_word(word, digits_in_word)
          words(from + i) = to_string(image_id, '(I2)')//"_"//word
-         word_lens(from + i) = image_id
       end do
 
       if (image_id == 1) then
          do i = 1, n_images
             from = (i - 1)*problem_size + 1
             to = i*problem_size
-            print *, "Last print:", from, to, i, words(:) [i], word_lens(:) [i]
+            print *, "Last print:", from, to, i, words(:) [i]
          end do
       end if
    end subroutine rm_duplicates_par
