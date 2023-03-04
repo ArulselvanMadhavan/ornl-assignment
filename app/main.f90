9880b9cc6700063160e959745828e6bed397d6b0
arul <arul@aruls-MacBook-Pro.local>
2023-03-03 21:40:26 -0800

    WIP

--- app/main.f90
+++ app/main.f90
@@ -1,26 +1,47 @@
 program main
    use ornl_assignment, only: extract_digits, remove_duplicates
+   use rand_utils, only: random_word
    use stdlib_string_type, only: string_type, assignment(=), write (formatted), char
+   use stdlib_strings, only: to_string
    use stdlib_hashmap_wrappers, only: fnv_1_hasher, key_type, set
    use stdlib_hashmaps, only: chaining_hashmap_type, int_index
    implicit none
-   character(len=5) :: id
-   character(len=:), allocatable :: id_clean
-   type(string_type) :: xs(3)
-   type(string_type), allocatable :: ys(:)
-   type(chaining_hashmap_type) :: map
+   integer, parameter :: problem_sizes(3) = [10, 50, 100]
+   integer, parameter :: word_len = 4
+   integer :: i, w, n_images, chunk, problem_size, image_id, from, to, neighbor
+   character(len=word_len) :: word
+   character(len=:), allocatable :: digits_in_word
+   type(string_type), allocatable, dimension(:), codimension[:] :: words
+   integer, dimension(10) :: word_lens[*]
+   integer :: stat
+   integer :: ending[*]
+   n_images = num_images()
+   image_id = this_image()
 
-   integer :: i
-   ! Problem 1
-   id = ".7324"
-   id_clean = extract_digits(id)
-
-   ! Problem 2
-   xs(1) = "arul"
-   xs(2) = "arul"
-   xs(3) = "a"
-   call remove_duplicates(xs, ys)
-   print *, xs
-   print *, ys
-   print *, char(xs(1))
+   do i = 1, size(problem_sizes)
+      problem_size = problem_sizes(i)
+      from = (image_id - 1)*problem_size + 1
+      to = image_id*problem_size
+      allocate (words(from:to) [*], stat=stat)
+      do w = 0, problem_size - 1
+         call random_word(word, digits_in_word)
+         words(from + w) = to_string(image_id)//"_"//word
+         word_lens(modulo(from + w, 10) + 1) = image_id
+      end do
+      neighbor = image_id + 2*modulo(image_id, 2) - 1
+      print *, problem_size, from, to, image_id, n_images, neighbor
+      ending = to
+      sync all
+      
+      ! if (image_id == 1) then
+      !    do w = 1, n_images
+      !       from = (w - 1)*problem_size + 1
+      !       to = w*problem_size
+      !       print *, "Last print:", from, to, w, words(:)[w]
+      !    end do
+      ! end if
+      print *, image_id, neighbor, word_lens(:)[neighbor], ending[image_id], ending[neighbor]
+      sync all
+      deallocate (words, stat=stat)
+   end do
 end program main
